79d9278a5f60926ffaaef6be5d855391b02da21c
Noura Shawky <81195332+nour671@users.noreply.github.com>
2024-11-10 19:52:27 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,258 @@
+
+var quotes = [ 
+  {
+  quote : "“Be yourself; everyone else is already taken.”" ,
+  author : " Oscar Wilde"
+}
+  ,
+  {
+  quote : "“So many books, so little time.”" ,
+  author : " Frank Zappa"
+}
+  ,
+  {
+  quote : "“A room without books is like a body without a soul.”" ,
+  author : " Marcus Tullius Cicero"
+}
+  ,
+  {
+  quote : "“You only live once, but if you do it right, once is enough.”" ,
+  author : " Mae West"
+}
+  ,
+  {
+  quote : "“In three words I can sum up everything I've learned about life: it goes on.”" ,
+  author : " Robert Frost"
+}
+  ,
+  {
+  quote : "“If you tell the truth, you don't have to remember anything.”" ,
+  author : " Mark Twain"
+}
+  ,
+  {
+  quote : "“A friend is someone who knows all about you and still loves you.”" ,
+  author : " Elbert Hubbard"
+}
+  ,
+  {
+  quote : "“Live as if you were to die tomorrow. Learn as if you were to live forever.”" ,
+  author : " Mahatma Gandhi"
+}
+  ,
+  {
+  quote : "“Be the change that you wish to see in the world.”",
+  author: "Mahatma Gandhi",
+}
+  ,
+  {
+  quote : "“I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”",
+  author: "Maya Angelou",
+}
+  ,
+  {
+  quote : "“To live is the rarest thing in the world. Most people exist, that is all.”",
+  author: "Oscar Wilde",
+}
+  ,
+  {
+  quote : "“Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.”",
+  author: "Martin Luther King Jr.",
+}
+  ,
+  {
+  quote : "“We accept the love we think we deserve.”",
+  author: "Stephen Chbosky",
+}
+  ,
+  {
+  quote : "“I am so clever that sometimes I don't understand a single word of what I am saying.”",
+  author: "Oscar Wilde",
+}
+  ,
+  {
+  quote : "“It is our choices, Harry, that show what we truly are, far more than our abilities.”",
+  author: "J.K. Rowling",
+}
+  ,
+  {
+  quote : "“There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle.”",
+  author: "Albert Einstein",
+}
+  ,
+  {
+  quote : "“It does not do to dwell on dreams and forget to live.”",
+  author: "J.K. Rowling",
+}
+  ,
+  {
+  quote : "“Good friends, good books, and a sleepy conscience: this is the ideal life.”",
+  author: "Mark Twain",
+}
+  ,
+  {
+  quote : "“We are all in the gutter, but some of us are looking at the stars.”",
+  author: "Oscar Wilde",
+}
+  ,
+  {
+  quote : "“Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.”",
+  author: "Bill Keane",
+}
+  ,
+  {
+  quote : "“I have not failed. I've just found 10,000 ways that won't work.”",
+  author: "Thomas A. Edison",
+}
+  ,
+  {
+  quote : "“A woman is like a tea bag; you never know how strong it is until it's in hot water.”",
+  author: "Eleanor Roosevelt",
+}
+  ,
+  {
+  quote : "“The man who does not read has no advantage over the man who cannot read.”",
+  author: "Mark Twain",
+}
+  ,
+  {
+  quote : "“The opposite of love is not hate, it's indifference. The opposite of art is not ugliness, it's indifference. The opposite of faith is not heresy, it's indifference. And the opposite of life is not death, it's indifference.”",
+  author: "Elie Wiesel",
+}
+  ,
+  {
+  quote : "“If you don't stand for something you will fall for anything.”",
+  author: "Gordon A. Eadie",
+}
+  ,
+  {
+  quote : "“I may not have gone where I intended to go, but I think I have ended up where I needed to be.”",
+  author: "Douglas Adams",
+}
+  ,
+  {
+  quote : "“I like nonsense, it wakes up the brain cells. Fantasy is a necessary ingredient in living.”",
+  author: "Dr. Seuss",
+}
+  ,
+  {
+  quote : "“All you need is love. But a little chocolate now and then doesn't hurt.”",
+  author: "Charles M. Schulz",
+}
+  ,
+  {
+  quote : "“Beauty is in the eye of the beholder and it may be necessary from time to time to give a stupid or misinformed beholder a black eye.”",
+  author: "Jim Henson",
+}
+  ,
+  {
+  quote : "“Today you are You, that is truer than true. There is no one alive who is Youer than You.”",
+  author: "Dr. Seuss",
+}
+  ,
+  {
+  quote : "“If one cannot enjoy reading a book over and over again, there is no use in reading it at all.”",
+  author: "Oscar Wilde",
+}
+  ,
+  {
+  quote : "“If you want your children to be intelligent, read them fairy tales. If you want them to be more intelligent, read them more fairy tales.”",
+  author: "Albert Einstein",
+}
+  ,
+  {
+  quote : "“Logic will get you from A to Z; imagination will get you everywhere.”",
+  author: "Albert Einstein",
+}
+  ,
+  {
+  quote : "“Sometimes people are beautiful. Not in looks. Not in what they say. Just in what they are.”",
+  author: "Markus Zusak",
+
+}
+  ,
+  {
+  quote : "“The truth is, everyone is going to hurt you. You just got to find the ones worth suffering for.”",
+  author: "Bob Marley",
+}
+  ,
+  {
+  quote : "“The more that you read, the more things you will know. The more that you learn, the more places you'll go.”",
+  author: "Dr. Seuss",
+}
+  ,
+  {
+  quote : "“Folks are usually about as happy as they make their minds up to be.”",
+  author: "Abraham Lincoln",
+}
+  ,
+  {
+  quote : "“There is no friend as loyal as a book.”",
+  author: "Ernest Hemingway",
+}
+  ,
+  {
+  quote : "“Success is not final, failure is not fatal: it is the courage to continue that counts.”",
+  author: "Winston S. Churchill",
+}
+  ,
+  {
+  quote : "“When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.”",
+  author: "Helen Keller",
+}
+  ,
+  {
+  quote : "“Love is like the wind, you can't see it but you can feel it.”",
+  author: "Nicholas Sparks",
+}
+  ,
+  {
+  quote : "“It is impossible to live without failing at something, unless you live so cautiously that you might as well not have lived at all—in which case, you fail by default.”",
+  author: "J.K. Rowling",
+}
+  ,
+  {
+  quote : "“To the well-organized mind, death is but the next great adventure.”",
+  author: "J.K. Rowling",
+}
+  ,
+  {
+  quote : "“Life isn't about finding yourself. Life is about creating yourself.”",
+  author: "George Bernard Shaw",
+}
+  ,
+  {
+  quote : "“It is not a lack of love, but a lack of friendship that makes unhappy marriages.”",
+  author: "Friedrich Nietzsche",
+}
+  ,
+  {
+  quote : "“Whenever you find yourself on the side of the majority, it is time to reform (or pause and reflect).”",
+  author: "Mark Twain",
+}
+  ,
+]
+
+
+
+
+// var previousQuote = 0 ;
+function getRandomQuote (){
+  var newQuote =  Math.floor( Math.random() * quotes.length );
+  
+  // if( previousQuote === newQuote){
+  //   newQuote =  Math.floor( Math.random() * quotes.length );
+  //   previousQuote = newQuote ;
+
+  // }
+
+ 
+
+  var quotesText = ` <p>${ quotes[newQuote].quote}</p> `
+  var quotesAuthor = ` <h2> - ${ quotes[newQuote].author} - </h2>`
+
+
+  document.getElementById("text").innerHTML = quotesText;
+  document.getElementById("author").innerHTML = quotesAuthor;
+
+};
